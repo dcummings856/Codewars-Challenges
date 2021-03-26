08930dc0396da04c2f37a8a5c9f136b32b28bf62
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-03-26 19:08:22 -0400

    one 8 kyu

--- script.js
+++ script.js
@@ -519,3 +519,13 @@ function enough(cap, on, wait) {
     return on + wait - cap
   }
 }
+
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+function createArray(number){
+  let newArray = [];
+  for(let i = 1; i <= number; i++){
+    newArray.push(i)
+  }
+  return newArray
+}
