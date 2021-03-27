f5e62db1b0f8fb253a565e4cd7de4d6ba7bcf3d4
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-03-27 14:25:43 -0400

    one 8 kyu

--- script.js
+++ script.js
@@ -529,3 +529,16 @@ function createArray(number){
   }
   return newArray
 }
+
+// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. 
+// First argument is an array of numbers and the second is the divisor.
+
+function divisibleBy(numbers, divisor){
+  let nums = []
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % divisor === 0) {
+      nums.push(numbers[i])
+    }
+  }
+  return nums
+}
