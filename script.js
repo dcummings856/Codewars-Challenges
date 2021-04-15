cb2826b39d518343c959b713922b01feba73847d
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-04-15 17:41:36 -0400

    one 8 kyu

--- script.js
+++ script.js
@@ -815,3 +815,17 @@ function xor(a, b) {
 function howManyLightsabersDoYouOwn(name) {
   return name == 'Zach' ? 18 : 0
 }
+
+// Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+// The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+// Upper or lower case letter does not matter -- "eNglisH" is also correct.
+// Return value as boolean values, true for the string to contains "English", false for it does not.
+
+function spEng(sentence){
+  if (sentence.toLowerCase().match(/english/g)) {
+    return true
+  } else {
+    return false
+  } 
+}
+
