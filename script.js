78790186ccdd02326bfd06a26087cca85990e79e
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-05-09 14:46:34 -0400

    two 8 kyus

--- script.js
+++ script.js
@@ -977,3 +977,28 @@ function swapValues() {
   args[0] = args[1];
   args[1] = temp;
 }
+
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+function getSize(width, height, depth) {
+  let volume = width * height * depth
+  let area = 2 * (height * width) + 2 * (height * depth) + 2 * (width * depth)
+  return [area, volume]
+}
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', 
+// if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+function well(x){
+  let filtered = x.filter(word => {
+    return word == "good"
+  })
+  console.log(filtered)
+  if (filtered.length <= 2 && filtered.length > 0) {
+    return 'Publish!'
+  } else if (filtered.length > 2) {
+    return 'I smell a series!'
+  } else {
+    return 'Fail!'
+  }
+}
