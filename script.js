819554d1b1c7f7d0fab8a30dbf249fad1caec3d1
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-05-14 18:58:48 -0400

    one 5 kyu

--- script.js
+++ script.js
@@ -1037,4 +1037,11 @@ function checkExam(array1, array2) {
   return total
 }
 
-// 
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string.
+
+function domainName(url){
+  url = url.replace("https://", '')
+  url = url.replace("http://", '')
+  url = url.replace("www.", '')
+  return url.split('.')[0]
+}
