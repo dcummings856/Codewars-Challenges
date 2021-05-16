b461359695055e9bfe14b1753837407d27ff41e0
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-05-16 13:55:35 -0400

    one 8 kyu

--- script.js
+++ script.js
@@ -1057,3 +1057,17 @@ function increasingNumber(x, n) {
       x++;
   return x;
 }
+
+// Your task is to implement the authenticate() method of the sleigh, which takes the name of the person, who wants to board the sleigh and a secret password. If, 
+// and only if, the name equals "Santa Claus" and the password is "Ho Ho Ho!" (yes, even Santa has a secret password with uppercase and lowercase letters and 
+// special characters :D), the return value must be true. Otherwise it should return false.
+
+function Sleigh() {}
+
+Sleigh.prototype.authenticate = function(name, password) {
+  if (name == "Santa Claus" && password === "Ho Ho Ho!") {
+    return true
+  } else {
+    return false
+  }
+};
