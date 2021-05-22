98044c0c235cdce1ad14aadbf5d91e22b272e5df
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-05-22 08:58:24 -0400

    one 7 kyu

--- script.js
+++ script.js
@@ -1116,3 +1116,19 @@ function multiTable(num) {
   }
   return table;
 }
+
+// There is a bus moving in the city, and it takes and drop some people in each bus stop.
+// You are provided with a list (or array) of integer arrays (or tuples). Each integer array has two items which represent number of people get into bus (The first item)
+// and number of people get off the bus (The second item) in a bus stop.
+// Your task is to return number of people who are still in the bus after the last bus station (after the last array). Even though it is the last bus stop, the bus is not
+// empty and some people are still in the bus, and they are probably sleeping there :D
+
+var number = function(busStops){
+  let on = 0
+  let off = 0
+  for (let i = 0; i < busStops.length; i++) {
+    on += busStops[i][0]
+    off += busStops[i][1]
+  }
+  return on - off
+}
