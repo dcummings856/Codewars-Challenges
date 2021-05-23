cb82e33c96c479349913efaf706e8c6a14eaa25e
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-05-23 18:38:12 -0400

    two 7 kyus

--- script.js
+++ script.js
@@ -1132,3 +1132,20 @@ var number = function(busStops){
   }
   return on - off
 }
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+function solution(str, ending){
+  return str.endsWith(ending);
+}
+
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a 
+// lower index. If you get an empty array/list, return an empty array/list.
+// Don't change the order of the elements that are left.
+
+function removeSmallest(numbers) {
+  numbers = numbers.slice(0);
+  const min = Math.min(...numbers);
+  numbers.splice(numbers.indexOf(min), 1);
+  return numbers;
+}
