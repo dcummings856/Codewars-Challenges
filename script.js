5d521a03e640f750171fa0e8790575195ea938bd
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-06-04 14:08:47 -0400

    one 8 kyu

--- script.js
+++ script.js
@@ -1244,3 +1244,9 @@ function plural(n) {
     return true
   }
 }
+
+// Your task is to return the correct string using the Template String Feature.
+
+var TempleStrings = function(obj, feature) {
+  return `${obj} are ${feature}`
+}
