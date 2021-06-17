e9fddfd13e103e7e502a8cfc5a96ea1c7876ae35
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-06-16 22:03:45 -0400

    8 kyu

--- script.js
+++ script.js
@@ -1283,3 +1283,4 @@ function isVow(a){
 
 // There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
 
+Object.freeze(MrFreeze)
