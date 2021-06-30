fdefd9d73409b64df440c1f83562e4a105b66392
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-06-30 18:08:59 -0400

    8 kyu

--- script.js
+++ script.js
@@ -1326,3 +1326,19 @@ function numberToPower(number, power){
   }
   return result
 }
+
+// You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
+
+function calculator(a,b,sign){
+  if (sign == "+") {
+    return a + b
+  } else if (sign == "-") {
+    return a - b
+  } else if (sign == "*") {
+    return a * b
+  } else if (sign == "/") {
+    return a / b
+  } else {
+    return "unknown value"
+  }
+}
