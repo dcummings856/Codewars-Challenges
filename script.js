346929683d1091ec9918898b6e8e66d040b72df0
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-08-02 16:42:08 -0400

    7 kyu

--- script.js
+++ script.js
@@ -1374,3 +1374,13 @@ function pointsPer48(ppg, mpg) {
   let number = ((48 / mpg) * ppg)
   return +number.toFixed(1) || 0
 }
+
+// Your task is to split the chocolate bar of given dimension n x m into small squares. Each square is of size 1x1 and unbreakable. Implement a function that will 
+// return minimum number of breaks needed.
+
+function breakChocolate(n,m) {
+  if (n * m == 0) {
+    return 0
+  }
+  return n * m -1
+}
