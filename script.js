58997e9b11dff2c82e8ca2a1caf0c6fe67dabb4f
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-08-06 19:03:16 -0400

    8 kyu

--- script.js
+++ script.js
@@ -1410,3 +1410,9 @@ function solution(nums){
     return a - b
   })
 }
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+function nameShuffler(str){
+  return str.split(' ').reverse().join(" ")
+}
