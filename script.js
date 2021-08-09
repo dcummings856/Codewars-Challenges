572344ccc7212e0720cc6fa23e62617aef459647
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-08-09 19:58:38 -0400

    7 kyu

--- script.js
+++ script.js
@@ -1416,3 +1416,14 @@ function solution(nums){
 function nameShuffler(str){
   return str.split(' ').reverse().join(" ")
 }
+
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
+// Assume that the input n will always be a positive integer.
+
+function sumCubes(n){
+  let result = 0
+  for (let i = 1; i <= n; i++) {
+    result += Math.pow(i, 3)
+  }
+  return result
+}
