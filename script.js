b4869b569afaef7e4e383f6c0317e6a2b5bb6938
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-08-11 20:40:21 -0400

    7 kyu

--- script.js
+++ script.js
@@ -1445,3 +1445,15 @@ function mergeArrays(a, b) {
    }
    return returnArray
 }
+
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, 
+// containing distinct letters - each taken only once - coming from s1 or s2.
+
+function longest(s1, s2) {
+  s3 = s1 + s2;
+  s4 = s3.split("")
+  s4 = s4.sort().filter(function(element, index, array){
+    return element !== array[index - 1]
+  })
+  return s4.join("")
+}
