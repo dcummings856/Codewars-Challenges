ca9578e0fad77bfe980485a39ffd7edf6b24e414
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-08-22 12:36:41 -0400

    7 kyu

--- script.js
+++ script.js
@@ -1495,3 +1495,13 @@ function arithmetic(a, b, operator){
 function golfScore(code) {
   return (code.match(/\w+|\S/g) || []).length
 }
+
+// We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond 
+// which the evaporator is no longer useful. All numbers are strictly positive.
+// The program reports the nth day (as an integer) on which the evaporator will be out of use.
+
+function evaporator(content, evap_per_day, threshold){ 
+  threshold = threshold / 100
+  evap_per_day = evap_per_day / 100
+  return  Math.ceil(Math.log(threshold) / Math.log(1-evap_per_day))
+}
