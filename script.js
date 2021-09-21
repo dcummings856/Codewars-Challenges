b9e3fab4eee4c224aed34d15ff86476fa3de681b
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-09-21 13:13:00 -0400

    8 kyu

--- script.js
+++ script.js
@@ -1543,4 +1543,17 @@ function fizzbuzz(n){
   return results
 }
 
- 
+// You can print your name on a billboard ad. Find out how much it will cost you. Each letter has a default price of £30, but that can be different if you are 
+// given 2 parameters instead of 1.
+// You can not use multiplier "*" operator.
+
+function billboard(name, price = 30){
+
+  var totalCost = 0;
+  for(i=0; i<name.length; i++){
+      totalCost += price;
+  } 
+  
+  return totalCost;
+  
+  }
