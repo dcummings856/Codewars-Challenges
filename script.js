0deaa645b518058fe842a31627dfc244860c2fc6
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-09-30 13:12:37 -0400

    8 kyu

--- script.js
+++ script.js
@@ -1557,3 +1557,11 @@ function billboard(name, price = 30){
   return totalCost;
   
   }
+
+  // Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater 
+  // than the sum of the cubes of each element in b.
+
+  function arrayMadness(a, b) {
+    return a.reduce( (sum, el) => sum + el ** 2, 0) >
+           b.reduce( (sum, el) => sum + el ** 3, 0)
+  }
