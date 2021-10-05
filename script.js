5d8981c5814e0e30a85ac4c77381f20205bdbfc2
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-10-05 10:41:00 -0400

    8 kyu

--- script.js
+++ script.js
@@ -1585,3 +1585,13 @@ function billboard(name, price = 30){
       return s 
     }
   }
+
+  // Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+  function combat(health, damage) {
+    if (health - damage < 0) {
+      return 0
+    } else {
+      return health - damage
+    }
+  }
