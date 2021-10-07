b369e3f256395d40d80fd7f002a02d562cae2a34
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-10-07 08:32:38 -0400

    7 kyu

--- script.js
+++ script.js
@@ -1595,3 +1595,13 @@ function billboard(name, price = 30){
       return health - damage
     }
   }
+
+  // In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+  function dontGiveMeFive(start, end){
+    let res = [];
+    for (let i = start; i <= end; i++) {
+      if (!i.toString().includes('5')) res.push(i);
+    }
+    return res.length;
+  }
