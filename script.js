bd97ee5fe60c099cd8df9efb509bbda55447ac56
David <dcummings856@gmail.com>
2021-10-09 12:06:20 -0400

    7 kyu

--- script.js
+++ script.js
@@ -1597,4 +1597,20 @@ function dontGiveMeFive(start, end){
   return res.length;
 }
  
-// 
+// Your task is to write function findSum.
+// Upto and including n, this function will return the sum of all multiples of 3 and 5.
+// For example:
+// findSum(5) should return 8 (3 + 5)
+// findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+function findSum(n) {
+  // loop through n
+  // check modulus of 3 and 5
+  // sum all multiples of 3 and 5
+  let sum = 0
+  for (let i = 1; i <= n; i++) {
+    if (i%3 == 0 || i%5 == 0) {
+      sum += i
+    }
+  }
+  return sum
+}
