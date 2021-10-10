9c2cc30ebbfe3f07ae4aa9ad6f74c8d574cb5c61
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-10-10 18:23:56 -0400

    8 kyu

--- script.js
+++ script.js
@@ -1614,3 +1614,15 @@ function findSum(n) {
   }
   return sum
 }
+
+
+// Write a function that rearranges an integer into its largest possible value.
+// Example (Input --> Output)
+// 123456 --> 654321
+// 105 --> 510
+// 12 --> 21
+// If the argument passed through is single digit or is already the maximum possible integer, your function should simply return it.
+
+function superSize(n){
+  return parseInt(n.toString().split('').sort().reverse().join(''))
+}
