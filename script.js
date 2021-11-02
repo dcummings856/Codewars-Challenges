557a6181ef48c9e4d1fd2e065567634bc96c85d1
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-11-02 12:40:55 -0400

    8 kyu

--- script.js
+++ script.js
@@ -1692,3 +1692,15 @@ function mystery() {
   }
   return results
 }
+
+// Functional closures can get overly attached. Set them straight!
+// Why doesn't greet_abe() actually greet Abe?
+
+var greet_abe = function() {
+  var name = 'Abe'
+  return "Hello, " + name + '!'
+}
+name = 'Ben';
+var greet_ben = function() {
+  return "Hello, " + name + '!'
+}
