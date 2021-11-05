1f5d4c019bba9717015e8b8922a6c4e62e229cfa
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-11-05 17:56:51 -0400

    8 kyu

--- script.js
+++ script.js
@@ -1716,3 +1716,21 @@ websites.push("codewars")
 function wrap(value) {
   return { value: value }
 }
+
+// Our counter prototype is broken. Can you spot, what's wrong here?
+
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase = function() {
+  this.value++;
+};
+
+Counter.prototype.getValue = function() {
+  return this.value;
+};
+
+Counter.prototype.reset = function() {
+  this.value = 0;
+};
