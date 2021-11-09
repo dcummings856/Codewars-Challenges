7f86acc3da9443efd71a040362da1253dd7a4e76
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-11-09 14:07:31 -0500

    8 kyu

--- script.js
+++ script.js
@@ -1740,3 +1740,15 @@ Counter.prototype.reset = function() {
 
 var websites = []
 while (websites.length < 1000) websites.push("codewars")
+
+// Shouldn't the two functions getMax1 and getMax2 be equivalent and return the same value? Can you spot a problem 
+// and fix it? Can you learn something about JavaScript style in this kata?
+
+function getMax1(){
+  var max = { name: 'Max Headroom' }
+  return max
+}
+
+function getMax2(){
+  return { name: 'Max Headroom' }
+}
