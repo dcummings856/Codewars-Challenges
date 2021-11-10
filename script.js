e6d6513dcb273451d8bec733f2c917d286e12048
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-11-10 17:12:36 -0500

    7 kyu

--- script.js
+++ script.js
@@ -1752,3 +1752,10 @@ function getMax1(){
 function getMax2(){
   return { name: 'Max Headroom' }
 }
+
+// The following code is not giving the expected results. Can you figure out what the issue is?
+// The following is an example of data that would be passed in to the function.
+
+function getNames(data){
+  return data.map(function(item){return item.name})
+}
