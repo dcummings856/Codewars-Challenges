51360d16c0c250e0e6bc9404d2c4bd8778950f50
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-11-13 10:09:36 -0500

    8 kyu

--- script.js
+++ script.js
@@ -1771,4 +1771,18 @@ Person.prototype.greet = function(yourName){
   return "Hello " + yourName + ", my name is " + this.name
 }
 
-//
+// VVhat ?!?
+// None of zese codevarriors seemz to remember hiz ovvn name !
+// Kould you help ?
+
+function Warrior(n){
+  let name = n;  
+  this.name = function(n){
+    if( n ) name=n;
+    return name;
+  }
+}
+  
+Warrior.prototype.toString = function(){
+    return "Hi! my name's "+this.name();
+}
