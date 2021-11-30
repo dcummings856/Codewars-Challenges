e8d8a1450255736d5f0f5d5dd274dface6409257
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-11-30 18:28:13 -0500

    7 kyu

--- script.js
+++ script.js
@@ -1787,4 +1787,11 @@ Warrior.prototype.toString = function(){
     return "Hi! my name's "+this.name();
 }
 
-// 
+// Input: Array of elements
+// ["h","o","l","a"]
+// Output: String with comma delimited elements of the array in th same order.
+// "h,o,l,a"
+
+function printArray(array){
+  return array.join();
+}
